aed1aa1d43eac7e1f1c20c40eabc2cbdf8c3b648
Christian Aust <git@kontakt.software-consultant.net>
2016-02-07 22:45:01 +0100

    Bootstrap fixes

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,8 @@ module Teamcontrol
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Berlin'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # The default locale is :de and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/models/**/*.yml').to_s]
     config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  resources :races
+
+  root to: 'races#index'
 end
