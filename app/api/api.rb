93dbaad4f5290f25de4d3137a07042782628be55
Christian Aust <git@kontakt.software-consultant.net>
2016-02-09 13:15:07 +0100

    Swagger fixes

--- app/api/api.rb
+++ app/api/api.rb
@@ -10,8 +10,15 @@ class API < Grape::API
   # mount Merchants::Categories
   # mount Merchants::Products
 
-  add_swagger_documentation hide_documentation_path:true, api_version:'v1', format: :json, info:{
-      description:'Data interface for TeamControl',
-      title:'TeamControl API'
+  add_swagger_documentation(
+    api_version:'v1',
+    base_path:'/api',
+    hide_documentation_path:true,
+    hide_format:true,
+    info:{
+      description:'Data interface for TeamControl stations.',
+      title:'TeamControl API',
+      contact:'kontakt@software-berater.net'
     }
+  )
 end
