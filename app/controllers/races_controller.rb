53ef50f0c803d52a56f91c2f8bc25a66e1949e52
David Eibich <david.eibich@software-berater.net>
2016-02-15 10:23:10 +0100

    Geplantes Renndatum

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -118,6 +118,6 @@ class RacesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def race_params
-    params.require(:race).permit :name, :duration, :max_drive, :max_turn, :break_time, :waiting_period
+    params.require(:race).permit :name, :duration, :max_drive, :max_turn, :break_time, :waiting_period, :scheduled
   end
 end
