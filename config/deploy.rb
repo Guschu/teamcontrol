a0c25e870d4645998f1cfabf348ec87b87f592cb
David Eibich <david.eibich@software-berater.net>
2016-02-19 17:35:10 +0100

    Vorbereitung für Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'TeamControl'
+set :repo_url, 'git@gitlab.software-consultant.net:swc/teamcontrol.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
@@ -11,7 +11,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
