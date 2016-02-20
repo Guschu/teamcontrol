0371355ef537c56b89ac0bbd422730c28e42f424
Christian Aust <git@kontakt.software-consultant.net>
2016-02-20 16:10:18 +0100

    Specs für helper

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe '#time_format' do
+    it 'formats time as a string' do
+      t = Time.now.change( hour:10, min:47, sec:51)
+      expect(helper.time_format(t)).to eq '10:47:51'
+    end
+
+    it 'returns nil if time is nil' do
+      expect(helper.time_format(nil)).to be_nil
+    end
+  end
+
+  describe '#seconds_format' do
+    it 'formats seconds as a string' do
+      expect(helper.seconds_format(17)).to eq '00:00:17'
+      expect(helper.seconds_format(67)).to eq '00:01:07'
+      expect(helper.seconds_format(23*60*60+17*60+23)).to eq '23:17:23'
+    end
+
+    it 'returns nil if seconds is nil' do
+      expect(helper.seconds_format(nil)).to be_nil
+    end
+  end
+
+  describe '#destroy_link_to' do
+    it 'generates a link' do
+      driver = create :driver
+      url = driver_url(driver)
+      expect(helper.destroy_link_to(url)).to match /data-method="delete"/
+      expect(helper.destroy_link_to(url)).to match /data-confirm="*"/
+    end
+  end
+end
