e0ef6b9e1f24e12f080535d00a04098a86d49d40
Christian Aust <git@kontakt.software-consultant.net>
2016-02-25 16:26:11 +0100

    Event specs

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -29,4 +29,74 @@ RSpec.describe Event, type: :model do
   end
 
   it { should define_enum_for :mode }
+
+  describe '#create before prebooking' do
+    let(:race) { create :race, prebooking_open:false }
+    let(:team) { create :team, race:race }
+    let(:attendance) { create :attendance, team:team }
+
+    it 'is not allowed' do
+      expect(race.aasm.current_state).to be :planned
+
+      evt = Event.create team_id:attendance.team_id, driver_id:attendance.driver_id
+      expect(evt).not_to be_persisted
+      expect(evt.errors).not_to be_empty
+    end
+  end
+
+  describe '#create while prebooking' do
+    let(:race) { create :race, prebooking_open:true }
+    let(:team) { create :team, race:race }
+    let(:team2) { create :team, race:race }
+    let(:attendance) { create :attendance, team:team }
+    let(:attendance2) { create :attendance, team:team }
+    let(:attendance3) { create :attendance, team:team2 }
+
+    it 'is allowed once per driver' do
+      expect(race.aasm.current_state).to be :planned
+
+      # Erster Fahrer
+      evt = Event.create team_id:attendance.team_id, driver_id:attendance.driver_id
+      expect(evt.errors).to be_empty
+      expect(evt).to be_persisted
+
+      # Erster Fahrer nochmal
+      Timecop.travel 5.seconds
+      evt = Event.create team_id:attendance.team_id, driver_id:attendance.driver_id
+      expect(evt.errors).not_to be_empty
+      expect(evt).to be_new_record
+    end
+
+    it 'is allowed once per team' do
+      expect(race.aasm.current_state).to be :planned
+
+      # erster Fahrer in Team 1
+      evt = Event.create team_id:attendance.team_id, driver_id:attendance.driver_id
+      expect(evt.errors).to be_empty
+      expect(evt).to be_persisted
+
+      # zweiter Fahrer des gleichen Teams
+      Timecop.travel 5.seconds
+      evt = Event.create team_id:attendance2.team_id, driver_id:attendance2.driver_id
+      expect(evt.errors).not_to be_empty
+      expect(evt).to be_new_record
+
+      # Fahrer einen anderen Teams
+      Timecop.travel 5.seconds
+      evt = Event.create team_id:attendance3.team_id, driver_id:attendance3.driver_id
+      expect(evt.errors).to be_empty
+      expect(evt).to be_persisted
+    end
+  end
+
+  describe '#create while race active' do
+    let(:race) { create :race, :started }
+    let(:team) { create :team, race:race }
+    let(:team2) { create :team, race:race }
+    let(:attendance) { create :attendance, team:team }
+    let(:attendance2) { create :attendance, team:team }
+    let(:attendance3) { create :attendance, team:team2 }
+
+    it ''
+  end
 end
