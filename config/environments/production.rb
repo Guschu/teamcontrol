cc103ed0393e0e95f862dec74762cc763aa8b467
JanVielhauer <jan@vielhauer.ruhr>
2023-02-12 13:05:02 +0100

    Try enforcing ssl in rails again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
 
-  config.force_ssl = false
-  config.ssl_options = { redirect: false }
+  config.force_ssl = true
+  config.ssl_options = { redirect: true }
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
